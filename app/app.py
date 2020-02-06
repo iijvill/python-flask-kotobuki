d439d69168a0b4001365b4666bfb028fdeac186e
iijvill <iijvillage@gmail.com>
2020-02-06 12:59:09 +0900

    ミニマムなformとpost機能追加

--- app/app.py
+++ app/app.py
@@ -1,19 +1,25 @@
 # flaskとrender_template(HRMLを表示させるための関数)をインポート
-from flask import Flask,render_template
+from flask import Flask, render_template, request
 
-#flaskオブジェクトの生成
+# flaskオブジェクトの生成
 
 app = Flask(__name__)
 
-#ルーティング。/にアクセスされたらhelloメソッドを実行する
+# ルーティング。/にアクセスされたらhelloメソッドを実行する
 @app.route("/")
-def hello():
-    return "Hello〜〜〜"
-
-#ルーティング。/indexへアクセスされたらindexメソッドを実行する
+# ルーティング。/indexへアクセスされたらindexメソッドを実行する
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    name = request.args.get("name")  # request.args.getでクエリストリングを受け取ることができる
+
+    # render_templateの引数に入れることでhtml側に送ることができる
+    return render_template("index.html", name=name)
+
+
+@app.route("/index", methods=["post"])
+def post():
+    name = request.form["name"]
+    return render_template("index.html", name=name)
 
 
 if __name__ == "__main__":
